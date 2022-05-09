d08bcc13307495fb864f1d5e32a5f259f668bc27
kwame anim <kwameanim@outlook.com>
2022-05-09 22:53:23 +0100

    generated stepdefination file

---
+++ Steps/ProductsSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace BDDAutomationTask.Steps
+{
+    [Binding]
+    public class ProductsSteps
+    {
+        [Given(@"I add four different products to my wish list")]
+        public void GivenIAddFourDifferentProductsToMyWishList()
+        {
+            
+        }
+        
+        [When(@"I view my wishlist table")]
+        public void WhenIViewMyWishlistTable()
+        {
+            
+        }
+        
+        [When(@"I search the lowest price product")]
+        public void WhenISearchTheLowestPriceProduct()
+        {
+           
+        }
+        
+        [When(@"I am able to add the lowest price to my cart")]
+        public void WhenIAmAbleToAddTheLowestPriceToMyCart()
+        {
+           
+        }
+        
+        [Then(@"I find total four selected items in my Wishlist")]
+        public void ThenIFindTotalFourSelectedItemsInMyWishlist()
+        {
+          
+        }
+        
+        [Then(@"I am able to verify the item in my cart")]
+        public void ThenIAmAbleToVerifyTheItemInMyCart()
+        {
+          
+        }
+    }
+}
